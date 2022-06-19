b3b3625a8f154c8e20ec5c9349a26d785a522dd9
timothyodonga <timothy.odonga@gmail.com>
2022-06-19 22:37:42 +0300

    Edited controlls component

--- src/components/Controlls.js
+++ src/components/Controlls.js
@@ -1,116 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import { Select, DatePicker } from "antd";
 import { observer } from "mobx-react";
 import { useStore } from "effector-react";
-import { $selectedDataElements, $store } from "../models/Store";
+import { $store } from "../models/Store";
 import {
-  setSelectedDataElementGroup,
-  onPolicyChange,
-  setSelectedDataElement,
   onPeriodChange,
+  setSelectedVariable,
+  setSelectedDistrict,
 } from "../models/Events";
-// import { useStore } from "../Context";
+import indicatorMeta from "../config/Indicators";
+
 const { Option } = Select;
 const { RangePicker } = DatePicker;
+
 const Controlls = () => {
   const store = useStore($store);
-  const selectedDataElements = useStore($selectedDataElements);
+  const [variableGroup, setVariableGroup] = useState("ALL");
+  const variableGroupSetList = [
+    ...new Set(indicatorMeta.map((val) => val.group)),
+  ];
+
+  console.log(store.period);
+
   return (
     <Row className="p-3">
       <Col>
         <Row style={{ marginBottom: 20 }}>
           <Col>
-            <Row>
-              <Col style={{ width: "100%" }}>
-                <p className="text-left label">SELECT AN INDICATOR</p>
-              </Col>
-            </Row>
+            {store.page != "overview" && (
+              <>
+                <Row>
+                  <Col style={{ width: "100%" }}>
+                    <p className="text-left label">SELECT AN INDICATOR GROUP</p>
+                  </Col>
+                </Row>
+                <Row style={{ marginBottom: 10 }}>
+                  <Col>
+                    <Select
+                      size="large"
+                      value={variableGroup}
+                      style={{ width: "100%" }}
+                      onChange={(val) => setVariableGroup(val)}
+                    >
+                      {variableGroupSetList.map((i) => (
+                        <Option key={i} value={i}>
+                          {i}
+                        </Option>
+                      ))}
+                    </Select>
+                  </Col>
+                </Row>
 
-            <Row style={{ marginBottom: 10 }}>
-              <Col>
-                <Select
-                  size="large"
-                  value={store.selectedDataElementGroup}
-                  style={{ width: "100%" }}
-                  onChange={(val) => setSelectedDataElementGroup(val)}
-                >
-                  {store.dataElementGroups.map((i) => (
-                    <Option key={i.id} value={i.id}>
-                      {i.name}
-                    </Option>
-                  ))}
-                </Select>
-              </Col>
-            </Row>
-            <Row style={{ marginBottom: 10 }}>
-              <Col>
-                <Select
-                  size="large"
-                  value={store.selectedDataElement}
-                  style={{ width: "100%" }}
-                  onChange={(val) => setSelectedDataElement(val)}
-                >
-                  {selectedDataElements.map((i) => (
-                    <Option key={i.id} value={i.id}>
-                      {i.name}
-                    </Option>
-                  ))}
-                </Select>
-              </Col>
-            </Row>
-            {/* <Row style={{ marginBottom: 10 }}>
-                <Col>
-                  <Select
-                    size="large"
-                    value={store.selectedIndicator}
-                    style={{ width: "100%" }}
-                    onChange={store.onIndicatorChange}
-                  >
-                    {store.indicators.map((i) => (
-                      <Option key={i.id} value={i.id}>
-                        {i.name}
-                      </Option>
-                    ))}
-                  </Select>
-                </Col>
-              </Row> */}
+                <Row>
+                  <Col style={{ width: "100%" }}>
+                    <p className="text-left label">SELECT AN INDICATOR</p>
+                  </Col>
+                </Row>
+                <Row style={{ marginBottom: 10 }}>
+                  <Col>
+                    <Select
+                      size="large"
+                      value={store.selectedVariable}
+                      style={{ width: "100%" }}
+                      onChange={(val) => setSelectedVariable(val)}
+                    >
+                      {variableGroup != "ALL"
+                        ? indicatorMeta
+                            .filter((i) => i.group == variableGroup)
+                            .map((i) => (
+                              <Option key={i.id} value={i.key}>
+                                {i.displayName}
+                              </Option>
+                            ))
+                        : indicatorMeta.map((i) => (
+                            <Option key={i.id} value={i.key}>
+                              {i.displayName}
+                            </Option>
+                          ))}
+                    </Select>
+                  </Col>
+                </Row>
+              </>
+            )}
           </Col>
         </Row>
-        {store.shown && (
-          <Row style={{ marginBottom: 20 }}>
-            <Col>
-              <Row>
-                <Col style={{ width: "100%" }}>
-                  <p className="text-left label">SELECT DISTRICT</p>
-                </Col>
-              </Row>
 
-              <Row style={{ marginBottom: 10 }}>
-                <Col>
-                  <Select
-                    size="large"
-                    value={store.selectedDistrict}
-                    style={{ width: "100%" }}
-                    onChange={store.onDistrictChange}
-                  >
-                    {store.districts.map((i) => (
-                      <Option key={i.id} value={i.id}>
-                        {i.name}
-                      </Option>
-                    ))}
-                  </Select>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
+        {store.page != "overview" && (
+          <>
+            <Row style={{ marginBottom: 20 }}>
+              <Col>
+                <Row>
+                  <Col style={{ width: "100%" }}>
+                    <p className="text-left label">SELECT DISTRICT</p>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col>
+                    <Select
+                      size="large"
+                      value={store.selectedDistrict}
+                      style={{ width: "100%" }}
+                      onChange={(val) => setSelectedDistrict(val)}
+                    >
+                      {store.districts.map((i) => (
+                        <Option key={i.id} value={i.id}>
+                          {i.name}
+                        </Option>
+                      ))}
+                    </Select>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </>
         )}
 
         <Row style={{ marginBottom: 20 }}>
           <Col>
             <Row>
               <Col style={{ width: "100%" }}>
-                <p className="text-left label">SELECT ANALYSIS TIMEFRAME</p>
+                <p className="text-left label">SELECT TIMEFRAME</p>
               </Col>
             </Row>
             <Row style={{ marginBottom: 10 }}>
@@ -122,40 +133,6 @@ const Controlls = () => {
                   value={store.period}
                   onChange={(val) => onPeriodChange(val)}
                 />
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-
-        <Row style={{ marginBottom: 20 }}>
-          <Col>
-            <Row>
-              <Col style={{ width: "100%" }}>
-                <p className="text-left label">
-                  SELECT OUTLIER POLICY
-                  <span className="material-icons align-middle logo-controll">
-                    info
-                  </span>
-                </p>
-              </Col>
-            </Row>
-            <Row style={{ marginBottom: 10 }}>
-              <Col>
-                <Select
-                  size="large"
-                  value={store.selectedPolicy}
-                  style={{ width: "100%" }}
-                  onChange={(val) => onPolicyChange(val)}
-                >
-                  <Option value="Keep outliers">Keep outliers</Option>
-                  <Option value="Correct outliers using SD">
-                    Correct outliers - using standard deviation
-                  </Option>
-                  <Option value="Correct outliers using ICR">
-                    Correct outliers - using interquartile range
-                  </Option>
-                  <Option value="Report">Report</Option>
-                </Select>
               </Col>
             </Row>
           </Col>
