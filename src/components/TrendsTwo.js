cff393bda82955180f3d870cb595bc65a9b7b4a5
timothyodonga <timothy.odonga@gmail.com>
2022-06-19 22:38:48 +0300

    Made changes to enable trend lineViz country

---
+++ src/components/TrendsTwo.js
@@ -0,0 +1,89 @@
+import { React, useEffect } from "react";
+import { Select } from "antd";
+import VisualizationHeader from "./VisualizationHeader";
+import { useStore } from "effector-react";
+import { $store } from "../models/Store";
+import { processCountryData } from "../utils";
+import LineVisualizationTwo from "./LineVisualizationTwo";
+import { useDataQuery } from "@dhis2/app-runtime";
+import { monthsBetween } from "../utils";
+import { setPage } from "../models/Events";
+import indicatorMeta from "../config/Indicators";
+
+const myQuery = {
+  results: {
+    resource: "analytics",
+    params: ({ variableId, period, orgLevel }) => ({
+      dimension: [
+        `dx:${variableId}`,
+        `ou:${orgLevel}`,
+        `pe:${monthsBetween(
+          period.map((p) => p.format("YYYY-MM" + "-01"))[0],
+          period.map((p) => p.format("YYYY-MM" + "-01"))[1]
+        ).join(";")}`,
+      ],
+      skipMeta: false,
+      paging: false,
+      includeNumDen: true,
+    }),
+  },
+};
+
+const { Option } = Select;
+
+const TrendsTwo = () => {
+  const store = useStore($store);
+  const variable = store.selectedVariable;
+  const period = store.period;
+  let variableId = "";
+  const variableObject = indicatorMeta.filter(
+    (i) => i.key == store.selectedVariable
+  )[0];
+
+  if (store.page != "trends") {
+    setPage("trends");
+  }
+
+  if (variableObject.function == "single") {
+    variableId = variableObject.numerator.dataElementId;
+  }
+  console.log(`Variable Id: ${variableId}`);
+
+  const districtQuery = useDataQuery(myQuery, {
+    variables: {
+      variableId: variableId,
+      period: period,
+      orgLevel: "LEVEL-3",
+    },
+  });
+
+  const districtLevelData = districtQuery.data;
+  const districtLevelError = districtQuery.error;
+  const districtLevelLoading = districtQuery.loading;
+  const districtLevelRefetch = districtQuery.refetch;
+
+  useEffect(() => {
+    districtLevelRefetch({ variableId: variableId, period: period });
+  }, [variableId, period]);
+
+  console.log(districtLevelData);
+  console.log(store.selectedVariable);
+
+  return (
+    <div id="ds-paginator">
+      <VisualizationHeader
+        icon="analytics"
+        title="Trends analysis over time, across districts and health facilities"
+        subTitle="Continuity of Essential Health Services"
+      />
+      <LineVisualizationTwo
+        data={districtLevelData}
+        loading={districtLevelLoading}
+        error={districtLevelError}
+        processor={processCountryData}
+      />
+    </div>
+  );
+};
+
+export default TrendsTwo;
