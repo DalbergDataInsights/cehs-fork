de3dc925836cf81473b81de4c70eb6245ee82a5f
timothyodonga <timothy.odonga@gmail.com>
2022-07-06 13:48:12 +0300

    Made LineVisualizationTwo only show country level Viz

--- src/components/LineVisualizationTwo.js
+++ src/components/LineVisualizationTwo.js
@@ -1,9 +1,15 @@
 import { useStore } from "effector-react";
-import { React } from "react";
+import React, { useMemo, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import Plot from "react-plotly.js";
 import { $store } from "../models/Store";
-import { processOrgDataTotal, processTitle } from "../utils";
+import {
+  computeCountryTimeSeries,
+  computeDistrictTimeSeries,
+  computeFacilityTimeSeries,
+  processOrgDataTotal,
+  processTitle,
+} from "../utils";
 import Download from "./Download";
 import Loading from "./Loading";
 import VisualizationTitle from "./VisualizationTitle";
@@ -34,100 +40,106 @@ const LineVisualizationTwo = ({
     .map((ou) => ou.name)[0];
   console.log(districtName);
 
-  const selectedDistrict = store.selectedDistrict;
+  // const selectedDistrict = store.selectedDistrict;
+  // let facility = null;
+  // let facilityName = "";
 
-  let dataViz = null;
-  let processedData = null;
-  let selectedDistrictData = null;
-  let facilitiesDataDict = {};
-  let facilityName = "";
-  let facility = null;
-  let districtFacilitiesData = null;
-
-  if (level == "country") {
-    if (data) {
-      if (data["results"]["rows"]) {
-        processedData = processOrgRawDataToTimeSeries(data["results"]["rows"]);
-      }
+  // let dataViz = null;
+  const dataViz = useMemo(() => {
+    if (level == "country") {
+      return computeCountryTimeSeries(data, level);
     }
-  }
+  }, [data]);
+
+  // const selectedDistrictData = useMemo(() => {
+  //   computeDistrictTimeSeries(data, districts, level, store.selectedDistrict);
+  // }, [data]);
+
+  // let facilitiesDataDict = {};
+  // let facilityName = "";
+  // let facility = null;
+  // // let districtFacilitiesData = null;
+
+  // if (level == "country") {
+  //   if (data) {
+  //     if (data["results"]["rows"]) {
+  //       processedData = processOrgRawDataToTimeSeries(data["results"]["rows"]);
+  //     }
+  //   }
+  // }
 
   // ================================================================================
-  if (level == "district") {
-    const districtIds = districts.map((val) => val.id);
-    const districtData = {};
-    if (data) {
-      if (data["results"]["rows"]) {
-        districtIds.map((id) => {
-          districtData[`${id}`] = data["results"]["rows"].filter(
-            (val) => val[1] == id
-          );
-        });
-      }
-    }
-    selectedDistrictData = districtData[store.selectedDistrict];
-  }
+  // if (level == "district") {
+  //   const districtIds = districts.map((val) => val.id);
+  //   const districtData = {};
+  //   if (data) {
+  //     if (data["results"]["rows"]) {
+  //       districtIds.map((id) => {
+  //         districtData[`${id}`] = data["results"]["rows"].filter(
+  //           (val) => val[1] == id
+  //         );
+  //       });
+  //     }
+  //   }
+  //   selectedDistrictData = districtData[store.selectedDistrict];
+  // }
 
   // ===========================
 
-  if (level == "facility") {
-    const districtFacilities =
-      districtFacilitiesMeta[selectedDistrict]["facility_ids"];
+  // if (level == "facility") {
+  //   const districtFacilities =
+  //     districtFacilitiesMeta[selectedDistrict]["facility_ids"];
 
-    if (data) {
-      if (data["results"]["rows"]) {
-        districtFacilitiesData = data["results"]["rows"].filter((val) =>
-          districtFacilities.includes(val[1])
-        );
-      }
-    }
+  //   if (data) {
+  //     if (data["results"]["rows"]) {
+  //       districtFacilitiesData = data["results"]["rows"].filter((val) =>
+  //         districtFacilities.includes(val[1])
+  //       );
+  //     }
+  //   }
 
-    if (districtFacilitiesData) {
-      districtFacilities.map((id) => {
-        facilitiesDataDict[`${id}`] = districtFacilitiesData.filter(
-          (val) => val[1] == id
-        );
-      });
-    }
+  //   if (districtFacilitiesData) {
+  //     districtFacilities.map((id) => {
+  //       facilitiesDataDict[`${id}`] = districtFacilitiesData.filter(
+  //         (val) => val[1] == id
+  //       );
+  //     });
+  //   }
 
-    // Now with the facility raw data for the facilities in the district
-    // Get the totals per facility
-    const facilitiesDataTotals = {};
-    Object.entries(facilitiesDataDict).forEach(([key, value]) => {
-      facilitiesDataTotals[key] = processOrgDataTotal(value);
-    });
+  //   // Now with the facility raw data for the facilities in the district
+  //   // Get the totals per facility
+  //   const facilitiesDataTotals = {};
+  //   Object.entries(facilitiesDataDict).forEach(([key, value]) => {
+  //     facilitiesDataTotals[key] = processOrgDataTotal(value);
+  //   });
 
-    const sortedData = sortDictionary(facilitiesDataTotals);
-    facility = sortedData.slice(0, 1).map((v) => v[0])[0];
-    facilityName = facilitiesMeta[facility];
-  }
+  //   const sortedData = sortDictionary(facilitiesDataTotals);
+  //   facility = sortedData.slice(0, 1).map((v) => v[0])[0];
+  //   facilityName = facilitiesMeta[facility];
+  // }
 
   // ======================================================================
-  dataViz =
-    level == "country"
-      ? processedData
-      : level == "district"
-      ? selectedDistrictData !== undefined &&
-        processOrgRawDataToTimeSeries(selectedDistrictData)
-      : facilitiesDataDict !== {} &&
-        facility !== null &&
-        processOrgRawDataToTimeSeries(facilitiesDataDict[facility]);
+  // dataViz =
+  //   level == "country"
+  //     ? processedData
+  //     : level == "district"
+  //     ? // ? selectedDistrictData !== undefined &&
+  //       selectedDistrictData !== undefined &&
+  //       processOrgRawDataToTimeSeries(selectedDistrictData)
+  //     : facilitiesDataDict !== {} &&
+  //       facility !== null &&
+  //       processOrgRawDataToTimeSeries(facilitiesDataDict[facility]);
 
   return (
     <>
       {loading && <Loading />}
-      {data && !loading && dataViz && (
+      {!loading && dataViz && (
         <Row className="data-card shadow-sm mb-5 rounded">
           <Col className="m-bot-24">
             {level == "country" && (
               <>
                 <VisualizationTitle
-                  analysis={processTitle(
-                    periods[0],
-                    periods[1],
-                    processOrgRawDataToTimeSeries(data["results"]["rows"]),
-                    ""
-                  )}
+                  analysis={processTitle(periods[0], periods[1], dataViz, "")}
                   what="Overview:"
                   indicatorDescription={displayName}
                   level="Across the country"
@@ -138,34 +150,6 @@ const LineVisualizationTwo = ({
                   </Col>
                 </Row>
               </>
-            )}
-            {level == "district" && (
-              <>
-                <VisualizationTitle
-                  analysis={processTitle(
-                    periods[0],
-                    periods[1],
-                    processOrgRawDataToTimeSeries(selectedDistrictData),
-                    ""
-                  )}
-                  what={`Deep-dive in ${districtName}`}
-                  indicatorDescription={displayName}
-                  level=""
-                />
-                <Row style={{ marginBottom: 20 }}>
-                  <Col className="graph">
-                    <h5>{`Total number of ${displayName} in ${districtName}`}</h5>
-                  </Col>
-                </Row>
-              </>
-            )}
-
-            {level == "facility" && (
-              <Row style={{ marginBottom: 20 }}>
-                <Col className="graph">
-                  <h5>{`Evolution of number of ${displayName} in ${facilityName}`}</h5>
-                </Col>
-              </Row>
             )}
 
             <Row>
