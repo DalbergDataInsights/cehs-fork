5f33d8e19cc08b032dc460f09535ff51a0522e07
timothyodonga <timothy.odonga@gmail.com>
2022-09-02 13:56:06 +0300

    Removed unused imports in LineVisualizationFacility

--- src/components/LineVisualizationFacility.js
+++ src/components/LineVisualizationFacility.js
@@ -4,18 +4,13 @@ import { Col, Row } from "react-bootstrap";
 import Plot from "react-plotly.js";
 import { $store } from "../models/Store";
 import {
-  computeCountryTimeSeries,
-  computeDistrictTimeSeries,
   computeFacilityTimeSeries,
   processOrgDataTotal,
-  processTitle,
   getKeyByValue,
 } from "../utils";
 import Download from "./Download";
 import Loading from "./Loading";
-import VisualizationTitle from "./VisualizationTitle";
 import { processOrgRawDataToTimeSeries, sortDictionary } from "../utils";
-import indicatorMeta from "../config/Indicators";
 import districtFacilitiesMeta from "../config/DistrictFacilities";
 import facilitiesMeta from "../config/Facilities";
 
