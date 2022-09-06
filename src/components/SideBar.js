874148b742b0e3a7a095baf4ade3d931ddd907f0
Assumpta Wanyama <wanyamaassumpta@gmail.com>
2022-09-06 12:06:09 +0300

    added switch button

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Controlls from "./Controlls";
 import Navigation from "./Navigation";
+import { Button } from "antd"
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
+  const [emailView, setEmailView] = useState(false)
+
+
   return (
     <div className="sidebar">
       <Navigation />
       <Controlls />
+      <Button style={{display: "flex", position:"absolute", bottom:"0px", justifyContent:"center"}}>
+        <Link to="/spph/template/edit">GO TO SPPH</Link>
+      </Button>
     </div>
   );
 };
